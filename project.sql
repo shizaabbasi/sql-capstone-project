31a3cb0b15403cb156fbbdeee5bae0c086ec8662
shizaabbasi <shizaabbasi0@gmail.com>
2025-05-03 20:47:22 +0500

    Add files via upload

--- project.sql
+++ project.sql
@@ -1,14 +1,25 @@
-create table operators(
-itemcode int,
-itemname text,
-batchcode text,
-coname text
+create table harsh(
+e_name text,
+product text,
+p_country text,
+export_c text
 );
-insert into operators(itemcode,itemname,batchcode,coname)
+insert into harsh(e_name,product,p_country,export_c)
 VALUES
-(1001,'chocolate','dm/2007-08/wbm',''),
-(1003,'hot dog','dm/2007-08/wb1','abj enterprise'),
-(1002,'condensed milk','dm/2007-08/wbm2','abj concern');
-SELECT * FROM operators WHERE coname = '';
-SELECT * FROM operators WHERE coname IS NOT NULL;
-
+('jordan','phone','south korea','u.s'),
+('amelia','car','sweden','uae'),
+('leo','bookcase','china','us'),
+('sienna','toyota corolla','italy','russia'),
+('aiden','energy drink','united states','mexico'),
+('morgan','huawei','japan','u.s'),
+('aurora','tim hortons','canada','china'),
+('gloria','rolex','switzerland','germany'),
+('kai','loreal','france','uae'),
+('isaac','coca-cola','united states','pakistan');
+SELECT * from harsh;
+SELECT DISTINCT p_country from harsh;
+SELECT DISTINCT product from harsh;
+SELECT DISTINCT export_c from harsh;
+SELECT * from harsh where e_name LIKE '%a';
+SELECT * from harsh where e_name LIKE 'a%';
+SELECT * from harsh where e_name LIKE '%or%';
